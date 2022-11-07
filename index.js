9e9bcbd9a53cc11f2317a661bd06a9f8ae9c92d3
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-11-07 21:23:18 +0600

    basic server done

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-
+const express = require('express');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+})
+
+app.listen(port, () => {
+    console.log('server is running on port', port);
+})
+
+
+
+
 const uri = "mongodb+srv://<username>:<password>@cluster0.jnuj2ye.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
