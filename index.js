f174545b67e85e5c9448bf8b73c729d67c1e0e25
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-11-07 21:25:21 +0600

    monodb added

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
-})
 
-app.listen(port, () => {
-    console.log('server is running on port', port);
-})
-
-
-
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.jnuj2ye.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.jnuj2ye.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
+    console.log('connected')
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
     client.close();
 });
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+})
+
+
+
+
+app.listen(port, () => {
+    console.log('server is running on port', port);
+})
