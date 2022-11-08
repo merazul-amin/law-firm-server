cb1ddfbdb2c20fcf3f0809a34b35f6ae7d4d9d4b
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-11-08 08:08:38 +0600

    services added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,19 +10,28 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_password}@cluster0.jnuj2ye.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('connected')
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    const serviceCollection = client.db('assignment11').collection('services');
+
+    app.get('/services', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    })
+}
+run().catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
     res.send('Server is running');
 })
+
+
 
 
 
